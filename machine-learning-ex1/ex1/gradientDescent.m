1f3f4deed556c87b3ea69aaa58f66af38c60047f
Nikolas Hunt <nikolashunt@protonmail.ch>
2021-03-18 22:50:32 +0100

    Ex1 mandatory completee

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,13 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+   
+    
+    theta_1 = theta(1) - ((alpha/m) .* sum((X*theta-y).*X(:, 1)));
+    theta_2 = theta(2) - ((alpha/m) .* sum((X*theta-y).*X(:, 2)));
+    theta(1) = theta_1;
+    theta(2) = theta_2;
+    
     % ============================================================
 
     % Save the cost J in every iteration    
