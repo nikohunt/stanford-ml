092a955530ca027f10af7a983c2977f9ae690a86
Nikolas Hunt <nikolashunt@protonmail.ch>
2021-03-20 18:37:24 +0100

    Add alpha loop plots

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -83,31 +83,50 @@ fprintf('Running gradient descent ...\n');
 fprintf('%d training features ...\n', size(X, 2));
 
 % Choose some alpha value
-alpha = 0.1;
+% alpha = 0.1;
+
+% Setup alpha values
+alpha = [0.1, 0.3, 0.01];
 num_iters = 50;
-fprintf('%d learning rate ...\n', alpha);
+%fprintf('%d learning rate ...\n', alpha);
 fprintf('%d iterations ...\n', num_iters);
 
-% Init Theta and Run Gradient Descent 
-theta = zeros(3, 1);
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
-% Plot the convergence graph
+
+
+% Init plot
 figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
-xlabel('Number of iterations');
-ylabel('Cost J');
+leg = {};
+counter = 1;
 
-% Display gradient descent's result
-fprintf('Theta computed from gradient descent: \n');
-fprintf(' %f \n', theta);
-fprintf('\n');
+for i = alpha;
+    
+  % Init Theta, cost results, and Run Gradient Descent 
+  theta = zeros(3, 1);
+  J_histories = zeros(length(alpha),1);
+  
+  [theta, J_history] = gradientDescentMulti(X, y, theta, i, num_iters);
 
+  % Plot the convergence graphs
+  line_color = rand;
+  plot(1:numel(J_history), J_history, line_color, 'LineWidth', 2);
+  xlabel('Number of iterations');
+  ylabel('Cost J');
+  leg{counter} = num2str(i);
+  hold on;
+  
+  % Display gradient descent's result
+  fprintf('Theta computed from gradient descent: \n');
+  fprintf(' %f \n', theta);
+  fprintf('\n');
+  counter++;
+end
+legend(leg);
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = 10; % You should change this
 
 
 % ============================================================
