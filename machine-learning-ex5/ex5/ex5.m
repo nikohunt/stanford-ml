bb237db1afa01aa60a8803cd9bf30098b1e32587
Nikolas Hunt <nikolashunt@protonmail.ch>
2021-04-29 12:10:09 +0200

    Complete ex4 learningCurve

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -35,12 +35,12 @@ load ('ex5data1.mat');
 m = size(X, 1);
 
 % Plot training data
-%plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-%xlabel('Change in water level (x)');
-%ylabel('Water flowing out of the dam (y)');
+plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+xlabel('Change in water level (x)');
+ylabel('Water flowing out of the dam (y)');
 
 fprintf('Program paused. Press enter to continue.\n');
-%pause;
+pause;
 
 %% =========== Part 2: Regularized Linear Regression Cost =============
 %  You should now implement the cost function for regularized linear 
@@ -69,7 +69,7 @@ fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
          grad(1), grad(2));
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+%pause;
 
 
 %% =========== Part 4: Train Linear Regression =============
